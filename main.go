c707c30a87f576a3bc7fb23b5db8c6545d84cbc0
Dmitrii Babikov <dmitrii.babikov@id.thesoul.io>
2024-10-19 14:38:02 +0300

    update

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
