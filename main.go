adc3e181420ea553ef8b7781d7804f6967bd0efd
Dmitrii Babikov <dmitrii.babikov@id.thesoul.io>
2024-10-19 15:05:19 +0300

    update

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! I am updated for Blue/Green!")
+	fmt.Fprintf(w, "Hello, World! I am updated for CodePipeline!")
 }
 
 func main() {
